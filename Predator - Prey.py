85e05a852ac80bbd11ec4a7a1cf0791c5e43b08e
georgioi <32482917+georgioi@users.noreply.github.com>
2022-06-25 23:39:48 +0300

    Update Predator - Prey.py

--- Predator - Prey.py
+++ Predator - Prey.py
@@ -1,4 +1,4 @@
-import random as r
+import random as rd
 import math as m
 import time as t
 import numpy as np
@@ -19,6 +19,8 @@ def f(x):
         for i in range (dimensions):
             result = result + (x[i]**2 - 10*m.cos(2*m.pi*x[i]))     #Rastrigin
         result = result + 10*dimensions
+    elif function == 'E' or function == 'e':
+        result = -m.cos(x[0])*m.cos(x[1])*m.exp(-(x[0]-m.pi)**2 - (x[1]-m.pi)**2)   #Easom
     return result
 
 
@@ -32,12 +34,12 @@ class Particles():
         self.position = position[:]                 #Particle's position
         self.velocity = velocity[:]                 #Particle's velocity
         self.best = self.position[:]                #Best self position
-        self.positionFitness = 0                    #Current position fitness
-        self.bestFitness = 0                        #Best self position fitness
+        self.positionFitness = f(self.position)     #Current position fitness
+        self.bestFitness = f(self.best)             #Best self position fitness
 
     #Check if current position is personal best
     def Best(self):
-        if self.positionFitness > self.bestFitness:
+        if self.positionFitness < self.bestFitness:
             self.best = self.position[:]
             self.bestFitness = self.positionFitness       
         
@@ -46,9 +48,9 @@ class Particles():
         for i in range(dimensions):
             self.position[i] = self.position[i] + self.velocity[i]
             
-    #Calculate particle's Fitness value according to formula F = 1/1+f (where F is Fitness)
+    #Calculate particle's Fitness value.
     def evaluate(self):
-        self.positionFitness = 1/(1+f(self.position))
+        self.positionFitness = f(self.position)
 
     #Set particles position, if it is out of boundaries, according to absorbing wall technique
     def fixPosition(self):
@@ -72,15 +74,15 @@ class Particles():
 
 class Predator(Particles):
 
-    #Constructor that calls base's class constructor and initialize variables used in the predator's velocity formula
+    #Constructor that calls base's class constructor.
     def __init__(self,position,velocity):
         super().__init__(position,velocity)
-        self.r = r.uniform(0,vmax[0])                  #Random variable used to update predators velocity
 
     #Calculate Predator's Velocity according to formula. preyBest Parameter is the best position of Preys swarm
     def setVelocity(self,preyBest):
+        r = [rd.uniform(0,vmax[i]) for i in range(dimensions)]           #Random variable used to update predators velocity
         for i in range(dimensions):
-            self.velocity[i] = self.velocity[i] + self.r* (preyBest[i] - self.position[i])  
+            self.velocity[i] = self.velocity[i] + r[i]* (preyBest[i] - self.position[i])  
         
 
 #Prey Class. This is subclass of particles class
@@ -93,14 +95,7 @@ class Prey(Particles):
         self.c1 = 2                 #Cognitive Coefficient
         self.c2 = 2                 #Social Coefficient
         self.c3 = 2.5               #Predator Coefficient
-        self.pf = r.uniform(0,1)    #Fear Propability
-        self.r1 = r.random()    
-        self.r2 = r.random()        #r1, r2, r3 and rn are random variables 
-        self.r3 = r.random()        #used to update preys velocity
-        self.rn = r.random()
-        self.a = 1                  #a and b are distance coefficients
-        self.b = 2
-
+       
     #Find the nearest particle element from predators list to self prey.
     def EuclideanDistance(self,predators):
         for i in range(len(predators)):
@@ -114,34 +109,40 @@ class Prey(Particles):
                 d = distance
         return d
     
-    #Calculate the measure of the effect that the predator has on the prey. d parameter is distance from nearest predator
-    def D(self,d):
-        return self.a*m.exp(-self.b*d)
+    #Calculate the measure of the effect that the predator has on the prey. d parameter is distance from nearest predator. a and b parameters are distance coefficients.
+    def D(self,d,a,b):
+        return a*m.exp(-b*d)
 
-    #Calculate Preys's Velocity according to formula. pBest parameter is prey's swarm best position and w parameter is inertia rate. d is  aparameter used for function D.
-    def setVelocity(self,pBest,d,w):
-        if self.rn > self.pf:
+    #Calculate Preys's Velocity according to formula. pBest parameter is prey's swarm best position and w parameter is inertia rate. d, a and b are  aparameters used for function D.
+    #pf parameter is fear propability
+    def setVelocity(self,pBest,d,a,b,w,pf):
+        rn = rd.uniform(0,1)
+        r1 = [rd.uniform(0,1) for i in range(dimensions)]    
+        r2 = [rd.uniform(0,1) for i in range(dimensions)]        #r1, r2, r3 and rn are random variables 
+        r3 = [rd.uniform(0,1) for i in range(dimensions)]        #used to update preys velocity
+
+        if rn <= pf:
             for i in range(dimensions):
-                self.velocity[i] =  w*self.velocity[i] + self.c1*self.r1*(self.best[i] - self.position[i]) + self.c2*self.r2*(pBest[i] - self.position[i])
+                self.velocity[i] =  w*self.velocity[i] + self.c1*r1[i]*(self.best[i] - self.position[i]) + self.c2*r2[i]*(pBest[i] - self.position[i]) + self.c3*r3[i]*self.D(d,a,b)
         else:
              for i in range(dimensions):
-                self.velocity[i] =  w*self.velocity[i] + self.c1*self.r1*(self.best[i] - self.position[i]) + self.c2*self.r2*(pBest[i] - self.position[i]) + self.c3*self.r3*self.D(d)
+                self.velocity[i] =  w*self.velocity[i] + self.c1*r1[i]*(self.best[i] - self.position[i]) + self.c2*r2[i]*(pBest[i] - self.position[i])
 
 
 #-----------------------------------------Main Function---------------------------------------------------
 
-E = 0.005       #Error
+E = 0.00001       #Error
 
 #Choose Objective function
 
-function = input("Choose Objective Function: A (Ackley) B(Booth), R(Rastrigin), S(Sphere): ")
+function = input("Choose Objective Function: A (Ackley) B(Booth), R(Rastrigin), S(Sphere), E(Easom): ")
 
 #Set number of dimensions
 
-if function == 'A' or function == 'a' or function == 'B' or function == 'b':
-    dimensions = 2
-else:                                                                               
+if function == 'R' or function == 'r' or function == 'S' or function == 's':
     dimensions = int(input("Give number of dimensions: "))
+else:                                                                                      
+    dimensions = 2
 
 #Define Prey-Predator ratio
 ratio = [4,1]
@@ -149,34 +150,40 @@ ratio = [4,1]
 if dimensions <= 2:
     D = 5
 elif dimensions<15:         #Size of predators swarm
-    D = 6                   #according to dimensions
+    D = 8                   #according to dimensions
 else:
-    D = 8
+    D = 20
+
 F = D*ratio[0]              #Size of preys swarm according to ratio
 
 S = D+F                     #Total number of particles
 
 #Set boundaries according to objective function. U upper boundary, L lower boundary
 
-if (function=='B' or function=='b'):
+if function=='B' or function=='b':
     U = [10,10]
     L = [-10,-10]
-elif (function=='A' or function == 'a' ):                                           
+elif function=='A' or function == 'a' :                                           
     U = [5,5]
     L = [-5,-5]
-elif (function == 'S' or function == 's'):
-    U = [15 for i in range(dimensions)]
-    L = [-15 for i in range(dimensions)]
-elif (function == 'R' or function == 'r'):
+elif function == 'S' or function == 's':
+    U = [10 for i in range(dimensions)]
+    L = [-10 for i in range(dimensions)]
+elif function == 'R' or function == 'r':
     U = [5.12 for i in range(dimensions)]
     L = [-5.12 for i in range(dimensions)]
+elif function == 'E' or function == 'e':
+    U = [100,100]
+    L = [-100,-100]
 
 #Set optimal solution according to objective function
 
 optimal = [0]*dimensions
 
-if (function=='B' or function=='b'):                    
-    optimal = [1,3] 
+if function == 'B' or function == 'b':                    
+    optimal = [1,3]
+elif function == 'E' or function == 'e':
+    optimal = [m.pi,m.pi]
 
 #Set maximum velocity limit
 
@@ -186,113 +193,135 @@ for i in range(dimensions):
 
 Wmin = 0.4          #Minimum inertia rate value
 Wmax = 0.9          #Maximum inertia rate value
+pfMax = 0.7         #Maximum Fear Propability
+pfMin = 0.3         #Minimum Fear Propability
+dfMax = 4           #Maximum Distance Coefficients value
+dfMin = 2           #Minimum Distance Coefficients value
+it = 500            #Maximum number of iterations
+
+table = []
+table1 = []
+table2 = []
 
 #------------------Algorithm------------------------
+for u in range(50):
+    start = t.perf_counter()
+    rd.seed(t.process_time())
+                
+    predators = []              #List of predator's particles
+    preys = []                  #List of prey's particles
 
-r.seed(t.process_time())
+    #Set initial particles position
+    positions = []
+    for i in range(S):
+        temp = [rd.uniform(L[i],U[i]) for i in range(dimensions)]
+        positions.append(temp)
+                
+    #Set initial particles velocity
+    velocities = []
+    for i in range(S):
+        temp = [rd.uniform(-vmax[i],vmax[i]) for i in range(dimensions)]
+        velocities.append(temp)
+
+    #Divide particles into predators and preys swarms
+    temp = 0
+    temp1 = 0
+
+    for i in range(S):
+        a = rd.randint(0,1)
+        if (a == 0 and temp !=D) or temp1==F:
+            aPredator = Predator(positions[i],velocities[i])
+            predators.append(aPredator)                             #Create Predators Particles and swarm
+            temp +=1
+
+        elif (a == 1 and temp1!=F) or temp==D:
+            aPrey = Prey(positions[i],velocities[i])
+            preys.append(aPrey)                                     #Create Preys Particles and swarm
+            temp1 +=1
+
+    predatorsBest = [0]*dimensions                          #Predators swarm best
+    preysBest = [0]*dimensions                              #Preys swarm best
+    globalBest = [0]*dimensions                             #Global swarm best
+
+    predatorsBestFitness = 0                                #Predators swarm best fitness value
+    preysBestFitness = 0                                    #Preys swarm best fitness value
         
-predators = []              #List of predator's particles
-preys = []                  #List of prey's particles
+    bestValues = []                                         #Best value of objective function for every iteration, used fot diagram
+    iterations = []                                         #How many iterations the algorithm run, used for diagram
+    
+    for i in range(it):
+                
+        #Calculate Predators swarm best and Fitness value for each predator
+        for j in range(D):
+            predators[j].evaluate()
+            predators[j].Best()
+            if j == 0:
+                predatorsBest = predators[j].best[:]
+                predatorsBestFitness = predators[j].bestFitness
+            elif predators[j].bestFitness < predatorsBestFitness:   
+                predatorsBest = predators[j].best[:]
+                predatorsBestFitness = predators[j].bestFitness
 
-#Set initial particles position
-positions = []
-for i in range(S):
-    temp = [r.uniform(L[i],U[i]) for i in range(dimensions)]
-    positions.append(temp)
+        #Calculate Preys swarm best and Fitness value for each prey
+        for j in range(F):
+            preys[j].evaluate()
+            preys[j].Best()
+            if j == 0:
+                preysBest = preys[j].best[:]
+                preysBestFitness = preys[j].bestFitness
+            elif preys[j].bestFitness < preysBestFitness:
+                preysBest = preys[j].best[:]
+                preysBestFitness = preys[j].bestFitness
+
+        #Calculate Global best
+        globalBest = predatorsBest[:]
+        if preysBestFitness < predatorsBestFitness:                 
+            globalBest = preysBest[:]
+
+        #Calculate Inertia Rate
+        w = Wmax - ((Wmax-Wmin)/it)*i
+
+        #Calclulate fear propability
+        pf = 0.99**i
+
+        #Calculate distance coefficient a
+        a = 0.99**i
+
+        #Calculate distance coefficient b
+        b = dfMin + ((dfMax-dfMin)/it)*i
+                
+        #Predators Velocity and Position Update    
+        for j in range(D):
+            predators[j].setVelocity(preysBest)                     
+            predators[j].fixVelocity()
+            predators[j].setPosition()
+            predators[j].fixPosition()
+
+        #Preys Velocity and Position Update
+        for j in range(F):
+            d = preys[j].EuclideanDistance(predators)              
+            preys[j].setVelocity(preysBest,d,a,b,w,pf)
+            preys[j].fixVelocity()
+            preys[j].setPosition()
+            preys[j].fixPosition()
+
+        bestValues.append(f(globalBest))                            
+        iterations.append(i)
         
-#Set initial particles velocity
-velocities = []
-for i in range(S):
-    temp = [r.uniform(-vmax[i],vmax[i]) for i in range(dimensions)]
-    velocities.append(temp)
+        #Calculate distance between optimal known solution and current best solution
 
-#Divide particles into predators and preys swarms
-temp = 0
-temp1 = 0
+        distance = abs(f(globalBest) - f(optimal))                  
 
-for i in range(S):
-    a = r.randint(0,1)
-    if (a == 0 and temp !=D) or temp1==F:
-        aPredator = Predator(positions[i],velocities[i])
-        predators.append(aPredator)                             #Create Predators Particles and swarm
-        temp +=1
+        #Test convergence criteria
+        if distance < E:
+            break
 
-    elif (a == 1 and temp1!=F) or temp==D:
-        aPrey = Prey(positions[i],velocities[i])
-        preys.append(aPrey)                                     #Create Preys Particles and swarm
-        temp1 +=1
+    end = t.perf_counter()
 
-predatorsBest = [0]*dimensions                          #Predators swarm best
-preysBest = [0]*dimensions                              #Preys swarm best
-globalBest = [0]*dimensions                             #Global swarm best
-
-predatorsBestFitness = 0                                #Predators swarm best fitness value
-preysBestFitness = 0                                    #Preys swarm best fitness value
-
-bestValues = []                                         #Best value of objective function for every iteration, used fot diagram
-iterations = []                                         #How many iterations the algorithm run, used for diagram
-
-for i in range(500):
-        
-    #Calculate Predators swarm best and Fitness value for each predator
-    for j in range(D):
-        predators[j].evaluate()
-        predators[j].Best()
-        if j == 0:
-            predatorsBest = predators[j].best[:]
-            predatorsBestFitness = predators[j].bestFitness
-        elif predators[j].bestFitness > predatorsBestFitness:   
-            predatorsBest = predators[j].best[:]
-            predatorsBestFitness = predators[j].bestFitness
-
-    #Calculate Preys swarm best and Fitness value for each prey
-    for j in range(F):
-        preys[j].evaluate()
-        preys[j].Best()
-        if j == 0:
-            preysBest = preys[j].best[:]
-            preysBestFitness = preys[j].bestFitness
-        elif preys[j].bestFitness> preysBestFitness:            
-            preysBest = preys[j].best[:]
-            preysBestFitness = preys[j].bestFitness
-
-    #Calculate Global best
-    globalBest = predatorsBest[:]
-    if preysBestFitness > predatorsBestFitness:                 
-        globalBest = preysBest[:]
-
-    #Calculate Inertia Rate
-    w = Wmax - ((Wmax-Wmin)/500)*i
-        
-    #Predators Velocity and Position Update    
-    for j in range(D):
-        predators[j].setVelocity(preysBest)                     
-        predators[j].fixVelocity()
-        predators[j].setPosition()
-        predators[j].fixPosition()
-
-    #Preys Velocity and Position Update
-    for j in range(F):
-        d = preys[j].EuclideanDistance(predators)              
-        preys[j].setVelocity(preysBest,d,w)
-        preys[j].fixVelocity()
-        preys[j].setPosition()
-        preys[j].fixPosition()
-     
-    bestValues.append(f(globalBest))                            
-    iterations.append(i)
-
-    #Calculate distance between optimal known solution and current best solution 
-    distance = 0
-        
-    for j in range(dimensions):
-        distance = distance + (globalBest[j] - optimal[j])**2                  
-    distance = m.sqrt(distance)
-
-    #Test convergence criteria
-    if distance < E:
-        break                                                           
-
+    table.append(f(globalBest))
+    table1.append(i+1)
+    table2.append(end-start)
+    
 #Best position and best position value
 print("Global Best:", globalBest,"\nGlobal Best value:",f(globalBest))
  
@@ -304,7 +333,31 @@ Y = np.array(bestValues)
 plt.plot(X,Y)
 plt.xlabel("Iterations")                                
 plt.ylabel("f Optimal Solution")
+plt.title("Easom")
 plt.grid()
 plt.show()
 
+#-------------------For tests---------------------
+average = sum(table)/50
+averageIt = sum(table1)/50
+averageDur = sum(table2)/50
+dif = [abs(i - f(optimal)) for i in table]
+min_index = dif.index(min(dif))
+max_index = dif.index(max(dif))
+best = table[min_index]
+worst = table[max_index]
+
+file = []
+file.append("\t\tSphere 30D")
+file.append("Average Solution: "+str(average))
+file.append("Best Solution: "+str(best))
+file.append("Worst Solution: "+str(worst))
+file.append("Average number of Iterations: "+str(averageIt))
+file.append("Maximum Iterations: "+str(it))
+file.append("Average Duration: "+str(averageDur)+"\n")
+    
+f = open("1ο Σετ.txt","a")
+for line in file:
+    f.write(line+"\n")
+f.close()
 
